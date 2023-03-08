1c6d5c467b15edf792230dc37346b35891c7b4b6
IyuninVA <IyuninVA@bk.ru>
2023-03-08 16:39:36 +0400

    add_Ex-26

---
+++ Ex-26.py
@@ -0,0 +1,17 @@
+# Задача 26:  Напишите программу, которая на вход принимает два числа A и B,
+# и возводит число А в целую степень B с помощью рекурсии.
+
+# *Пример:*
+# A = 3; B = 5 -> 243 (3⁵)
+#     A = 2; B = 3 -> 8 
+
+def Exponentiation(number, degree):
+    if degree == 0:
+        return 1
+    return number * Exponentiation(number, degree-1)
+
+number = int(input('Введите число: '))
+degree = int(input('Введите степень числа: '))
+
+print(Exponentiation(number, degree))
+
